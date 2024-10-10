d3d381d832c4ff643d0c7e4d0a8f4cdbfcb5eec8
tatiana.manokhina <tmanokhi@yandex.ru>
2024-10-11 01:06:15 +0300

    Complete CatTest

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,26 +1,37 @@
 package com.example;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    @Mock
+    Feline feline;
+    Cat cat;
 
     @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+    public void setUp() {
+        cat = new Cat(feline);
     }
 
     @Test
     public void getSound() {
+        assertEquals("Мяу", cat.getSound());
     }
 
     @Test
-    public void getFood() {
+    public void getFood() throws Exception {
+        List<String> expectedFood = List.of("Яблоки", "Оливки", "Чипсы");
+        when(feline.eatMeat()).thenReturn(expectedFood);
+
+        assertEquals(expectedFood, cat.getFood());
     }
 }
