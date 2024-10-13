ab592b382fd7313709904904bb6808d5c65c4f32
tatiana.manokhina <tmanokhi@yandex.ru>
2024-10-13 23:06:24 +0300

    Format FelineTest

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,34 +1,43 @@
 package com.example;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
+    @Spy
+    Feline feline;
 
     @Test
-    public void eatMeat() {
+    public void eatMeat() throws Exception {
+        feline.eatMeat();
+        verify(feline, times(1)).getFood("Хищник");
     }
 
     @Test
     public void getFamily() {
+        assertEquals("Кошачьи", feline.getFamily());
     }
 
     @Test
     public void getKittens() {
+        feline.getKittens();
+        verify(feline, times(1)).getKittens(1);
     }
 
     @Test
-    public void testGetKittens() {
+    public void testKittensCount() {
+        assertEquals(feline.getKittens(1), feline.getKittens());
     }
 }
